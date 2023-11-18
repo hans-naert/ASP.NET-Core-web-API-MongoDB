02d43136e593f63f5d20bfeb31dd4b87b5beb2eb
Hans Naert <hans.naert@vives.be>
2023-11-18 13:31:22 +0100

    Change webapi from camel case to Pascal case

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ builder.Services.AddSingleton<BooksService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 
 var app = builder.Build();
 
