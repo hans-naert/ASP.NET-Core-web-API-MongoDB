7912ce2ba4513fe28261e00372f51a6a0e62c4b1
Hans Naert <hans.naert@vives.be>
2023-11-18 13:33:04 +0100

    Change BookName in webapi to Name

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,5 +1,6 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
 
 namespace BookStoreApi.Models;
 
@@ -10,6 +11,7 @@ public class Book
     public string? Id { get; set; }
 
     [BsonElement("Name")]
+    [JsonPropertyName("Name")]
     public string BookName { get; set; } = null!;
 
     public decimal Price { get; set; }
