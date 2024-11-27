eee64d3cb63e0c96eef5d42e7450a30e46fbb548
Hans Naert <hans.naert@vives.be>
2024-11-27 18:43:59 +0100

    Open swagger page by default

--- Program.cs
+++ Program.cs
@@ -33,6 +33,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
